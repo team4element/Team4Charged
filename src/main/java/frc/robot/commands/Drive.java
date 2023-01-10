4a49a93291df2b3f73ac4bed2124602a5ba06c6d
BrandonComins <brandcomin@gmail.com>
2023-01-09 18:50:27 -0800

    add barebones tank drive

---
+++ src/main/java/frc/robot/commands/Drive.java
@@ -0,0 +1,43 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot.commands;
+
+import edu.wpi.first.wpilibj2.command.CommandBase;
+import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
+import frc.robot.subsystems.DriveTrain;
+
+public class Drive extends CommandBase {
+  private final DriveTrain m_drive;
+  private final CommandXboxController controller;
+  
+  public Drive(DriveTrain drive, CommandXboxController controller) {
+    this.m_drive = drive;
+    this.controller = controller;
+
+    addRequirements(this.m_drive);
+  }
+
+  // Called when the command is initially scheduled.
+  @Override
+  public void initialize() {}
+
+  // Called every time the scheduler runs while the command is scheduled.
+  @Override
+  public void execute() {
+    double leftPower = this.controller.getLeftY();
+    double rightPower = this.controller.getLeftX();
+    m_drive.setPower(leftPower, rightPower);
+  }
+
+  // Called once the command ends or is interrupted.
+  @Override
+  public void end(boolean interrupted) {}
+
+  // Returns true when the command should end.
+  @Override
+  public boolean isFinished() {
+    return false;
+  }
+}
---
+++ src/main/java/frc/robot/subsystems/DriveTrain.java
@@ -0,0 +1,50 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot.subsystems;
+
+import com.ctre.phoenix.motorcontrol.can.WPI_TalonSRX;
+import com.ctre.phoenix.motorcontrol.can.WPI_VictorSPX;
+
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+
+public class DriveTrain extends SubsystemBase {
+  //Declare Motor Objects
+  private WPI_VictorSPX leftFront;
+  private WPI_VictorSPX leftMiddle;
+  private WPI_TalonSRX leftBack;
+
+  private WPI_VictorSPX rightFront;
+  private WPI_VictorSPX rightMiddle;
+  private WPI_TalonSRX rightBack;
+
+  public DriveTrain() {
+    //Define Motor Objects
+    leftFront = new WPI_VictorSPX(1);
+    leftMiddle = new WPI_VictorSPX(2);
+    leftBack = new WPI_TalonSRX(3);
+    
+    rightFront = new WPI_VictorSPX(4);
+    rightMiddle = new WPI_VictorSPX(5);
+    rightBack = new WPI_TalonSRX(6);
+
+    //Make motors Follow the Leader 
+    leftMiddle.follow(leftBack);
+    leftFront.follow(leftBack);
+
+    rightMiddle.follow(rightBack);
+    rightFront.follow(leftBack);
+
+  }
+
+  @Override
+  public void periodic() {
+    // This method will be called once per scheduler run
+  }
+
+  public void setPower(double leftPower, double rightPower){
+    leftBack.set(leftPower);
+    rightBack.set(rightPower);
+  }
+}
