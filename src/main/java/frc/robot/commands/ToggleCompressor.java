a34eec4f2bfea1e443e566ad739ee1f1ade545bc
Aaron Kraus <aaron.kraus38@gmail.com>
2023-02-23 18:58:40 -0800

    print pressure and isEnabled

--- src/main/java/frc/robot/commands/ToggleCompressor.java
+++ src/main/java/frc/robot/commands/ToggleCompressor.java
@@ -25,7 +25,8 @@ public class ToggleCompressor extends CommandBase {
   // Called every time the scheduler runs while the command is scheduled.
   @Override
   public void execute() {
-    System.out.println("pressed");
+    System.out.println(Arm.mCompressor.isEnabled());
+    System.out.println(Arm.mCompressor.getPressure());
   }
 
   // Called once the command ends or is interrupted.
