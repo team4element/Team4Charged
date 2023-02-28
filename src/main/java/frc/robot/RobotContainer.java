5118bb855abfc316b0f7545e68fe1f7ca041e747
Aaron Kraus <aaron.kraus38@gmail.com>
2023-02-27 18:18:08 -0800

    comment out constants import until needed again

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -4,7 +4,7 @@
 
 package frc.robot;
 
-import frc.robot.Constants;
+// import frc.robot.Constants;
 import frc.robot.commands.Autos;
 import frc.robot.commands.RotateToAngle;
 import frc.robot.commands.Drive;
