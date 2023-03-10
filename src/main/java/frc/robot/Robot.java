09d8579baf53d119a64b337bfbbbd88ebec4a9f2
Aaron Kraus <aaron.kraus38@gmail.com>
2023-03-10 10:10:41 -0800

    Bring back camera and remove useless comments

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -36,7 +36,7 @@ public class Robot extends TimedRobot {
     // Instantiate our RobotContainer. This will perform all our button bindings,
     // and put our
     // autonomous chooser on the dashboard.
-    // CameraServer.startAutomaticCapture();
+    CameraServer.startAutomaticCapture();
     m_robotContainer = new RobotContainer();
     RobotContainer.m_driveTrain.resetSensors();
   }
@@ -82,10 +82,7 @@ public class Robot extends TimedRobot {
   @Override
   public void autonomousInit() {
     m_autonomousCommand = m_robotContainer.getAutonomousCommand();
-
-    // m_autonomousCommand = Commands.sequence(
-    //     new Score(RobotContainer.m_intake),
-    //     new TaxiAuto(RobotContainer.m_driveTrain));
+    
     RobotContainer.m_arm.mCompressor.disable();
     // schedule the autonomous command (example)
     if (m_autonomousCommand != null) {
