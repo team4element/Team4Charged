91108c03c7e3b83bdc99f5658e6b3857d9485905
Aaron Kraus <aaron.kraus38@gmail.com>
2023-03-28 10:49:00 -0700

    + pivot to HighConePosition &+ PID for both states

---
+++ src/main/java/frc/robot/commands/HighConePosition.java
@@ -0,0 +1,19 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot.commands;
+
+import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
+
+import frc.robot.subsystems.Arm;
+
+public class HighConePosition extends SequentialCommandGroup {
+  private final Arm m_arm;
+
+  public HighConePosition(Arm arm) {
+    this.m_arm = arm;
+
+    addCommands(new TogglePivot(this.m_arm), new HoldArmPosition(this.m_arm, 110));
+  }
+}
--- src/main/java/frc/robot/commands/LowerArmDown.java
+++ src/main/java/frc/robot/commands/LowerArmDown.java
@@ -10,11 +10,13 @@ import edu.wpi.first.math.controller.PIDController;
 import edu.wpi.first.math.util.Units;
 import edu.wpi.first.wpilibj2.command.CommandBase;
 import frc.robot.Constants;
+import frc.robot.ElementMath;
 import frc.robot.subsystems.Arm;
 
 public class LowerArmDown extends CommandBase {
 
-  private PIDController armPID;
+  private PIDController normalArmPID;
+  private PIDController pivotedArmPID;
 
   private final Arm m_arm;
   
@@ -24,8 +26,17 @@ public class LowerArmDown extends CommandBase {
       Constants.ArmConstants.kV, Constants.ArmConstants.kA);
   
   public LowerArmDown(Arm arm) {
-
     this.m_arm = arm;
+
+    // if (this.m_arm.getPivotState()){
+    normalArmPID = new PIDController(Constants.ArmConstants.kArmP, Constants.ArmConstants.kArmI, Constants.ArmConstants.kArmD, feedForward.calculate(Math.cos(Units.degreesToRadians(Constants.ArmConstants.kBaseAngle)), 2, 2));
+    pivotedArmPID = new PIDController(Constants.ArmConstants.kArmP, Constants.ArmConstants.kArmI, Constants.ArmConstants.kArmD, feedForward.calculate(Math.cos(Units.degreesToRadians(Constants.ArmConstants.kPivotedAngle)), 2, 2));
+    // } else {
+    //   armPID = new PIDController(Constants.ArmConstants.kArmP, Constants.ArmConstants.kArmI, Constants.ArmConstants.kArmD, feedForward.calculate(Math.cos(Units.degreesToRadians(Constants.ArmConstants.kBaseAngle)), 2, 2));
+    // }
+  
+    normalArmPID.setTolerance(this.m_arm.armAngleToTicks(tolerance));
+    pivotedArmPID.setTolerance(this.m_arm.armAngleToTicks(tolerance));
 
     addRequirements(this.m_arm);
   }
@@ -33,28 +44,23 @@ public class LowerArmDown extends CommandBase {
   // Called when the command is initially scheduled.
   @Override
   public void initialize() {
-    if (this.m_arm.getPivotState()){
-      armPID = new PIDController(Constants.ArmConstants.kArmP, Constants.ArmConstants.kArmI, Constants.ArmConstants.kArmD, feedForward.calculate(Math.cos(Units.degreesToRadians(Constants.ArmConstants.kPivotedAngle)), 2, 2));
-    } else {
-      armPID = new PIDController(Constants.ArmConstants.kArmP, Constants.ArmConstants.kArmI, Constants.ArmConstants.kArmD, feedForward.calculate(Math.cos(Units.degreesToRadians(Constants.ArmConstants.kBaseAngle)), 2, 2));
-    }
-  
-    // armPID = new PIDController(Constants.ArmConstants.kArmP, Constants.ArmConstants.kArmI, Constants.ArmConstants.kArmD);
-  
-    armPID.setTolerance(this.m_arm.armAngleToTicks(tolerance));
-      
     this.m_arm.setArmPower(0);
   }
 
   // Called every time the scheduler runs while the command is scheduled.
   @Override
   public void execute() {
-    if (this.m_arm.getPivotState()){
-      double power = MathUtil.clamp(armPID.calculate(this.m_arm.getEncoderDistance(), this.m_arm.armAngleToTicks(Constants.ArmConstants.kPivotedAngle)), -0.5, 0.9);
-      this.m_arm.setArmPower(power);
+    if (this.m_arm.getControllerInput() > .1 || this.m_arm.getControllerInput() < -.1){
+      double power = ElementMath.cubeInput(this.m_arm.getControllerInput());
+      m_arm.setArmPower(power);
     } else {
-      double power = MathUtil.clamp(armPID.calculate(this.m_arm.getEncoderDistance(), this.m_arm.armAngleToTicks(Constants.ArmConstants.kBaseAngle)), -0.5, 0.9);
-      this.m_arm.setArmPower(power);
+      if (this.m_arm.getPivotState()){
+        double power = MathUtil.clamp(pivotedArmPID.calculate(this.m_arm.getEncoderDistance(), this.m_arm.armAngleToTicks(Constants.ArmConstants.kPivotedAngle)), -0.5, 0.9);
+        this.m_arm.setArmPower(power);
+      } else {
+        double power = MathUtil.clamp(normalArmPID.calculate(this.m_arm.getEncoderDistance(), this.m_arm.armAngleToTicks(Constants.ArmConstants.kBaseAngle)), -0.5, 0.9);
+        this.m_arm.setArmPower(power);
+      }
     }
   }
 
@@ -65,6 +71,9 @@ public class LowerArmDown extends CommandBase {
   // Returns true when the command should end.
   @Override
   public boolean isFinished() {
-    return armPID.atSetpoint();
+    // if (this.m_arm.getControllerInput() > .1 || this.m_arm.getControllerInput() < -.1){
+    //   return true;
+    // }
+    return false;
   }
 }
