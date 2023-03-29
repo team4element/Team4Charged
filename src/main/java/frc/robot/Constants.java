0dbc34bd3d208c91c405b9c4d4c5ba2fc407a171
Aaron Kraus <aaron.kraus38@gmail.com>
2023-03-28 17:01:59 -0700

    make path planner velocity 2.5

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -42,8 +42,8 @@ public final class Constants {
     public static final double kTrackWidthMeters = Units.inchesToMeters(29.5);
     public static final DifferentialDriveKinematics kDriveKinematics = new DifferentialDriveKinematics(kTrackWidthMeters);
     
-    public static final double kMaxSpeedMetersPerSecond = 3.0;
-    public static final double kMaxAccelerationPerSecondSquared = 3.0;
+    public static final double kMaxSpeedMetersPerSecond = 2.5;
+    public static final double kMaxAccelerationPerSecondSquared = 3;
 
     public static final double kRamseteB = 2;
     public static final double kRamseteZeta = 0.7;
