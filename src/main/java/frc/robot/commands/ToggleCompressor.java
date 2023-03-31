ea90864fa38fd46bde012feb3f40d9a65e753a2a
Aaron Kraus <aaron.kraus38@gmail.com>
2023-03-30 17:43:05 -0700

    Change compressor from hold to toggle

--- src/main/java/frc/robot/commands/ToggleCompressor.java
+++ src/main/java/frc/robot/commands/ToggleCompressor.java
@@ -19,25 +19,24 @@ public class ToggleCompressor extends CommandBase {
   // Called when the command is initially scheduled.
   @Override
   public void initialize() {
-    m_arm.mCompressor.enableDigital();
+    if (m_arm.mCompressor.isEnabled()) {
+      this.m_arm.mCompressor.disable();
+    } else {
+      this.m_arm.mCompressor.enableDigital();
+    }
   }
 
   // Called every time the scheduler runs while the command is scheduled.
   @Override
-  public void execute() {
-    System.out.println(m_arm.mCompressor.isEnabled());
-    System.out.println(m_arm.mCompressor.getPressure());
-  }
+  public void execute() {}
 
   // Called once the command ends or is interrupted.
   @Override
-  public void end(boolean interrupted) {
-    m_arm.mCompressor.disable();
-  }
+  public void end(boolean interrupted) {}
 
   // Returns true when the command should end.
   @Override
   public boolean isFinished() {
-    return false;
+    return true;
   }
 }
