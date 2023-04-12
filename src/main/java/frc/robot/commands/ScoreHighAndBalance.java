c5c59e719d3985ba9358fabd85b104cd5ed30d0d
Aaron Kraus <aaron.kraus38@gmail.com>
2023-04-12 15:25:15 -0700

    + High/Balance and compensate for -10 in High/Taxi

--- src/main/java/frc/robot/commands/ScoreHighAndBalance.java
+++ src/main/java/frc/robot/commands/ScoreHighAndBalance.java
@@ -6,22 +6,23 @@ import frc.robot.subsystems.DriveTrain;
 import frc.robot.subsystems.Arm;
 import frc.robot.subsystems.Intake;
 
-public class ScoreHighAndTaxiAuto extends SequentialCommandGroup {
+public class ScoreHighAndBalance extends SequentialCommandGroup {
   private final DriveTrain m_drive;
   private final Arm m_arm;
   private final Intake m_intake;
 
-  public ScoreHighAndTaxiAuto(DriveTrain drive, Arm arm, Intake intake) {
+  public ScoreHighAndBalance(DriveTrain drive, Arm arm, Intake intake) {
     this.m_drive = drive;
     this.m_arm = arm;
     this.m_intake = intake;
 
-    addCommands(new DriveToPosition(this.m_drive, -12).withTimeout(2),
-        new HighConePosition(this.m_arm).withTimeout(2),
+    addCommands(new HighConePosition(this.m_arm).withTimeout(2),
         new DriveToPosition(this.m_drive, 11.5).withTimeout(2),
-        new ToggleClaw(this.m_intake),
+        new ToggleClaw(this.m_intake), 
         new DriveToPosition(this.m_drive, -10).withTimeout(2),
         new TogglePivot(this.m_arm),
-        new LowerArmAndDrive(this.m_drive, this.m_arm, -180).withTimeout(5));
+        new LowerArmAndDrive(this.m_drive, this.m_arm, -138).withTimeout(5), 
+        new DriveToPosition(this.m_drive, 68).withTimeout(3),
+        new Balance(this.m_drive, true));
   }
 }
