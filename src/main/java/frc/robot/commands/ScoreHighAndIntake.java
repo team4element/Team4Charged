fd73339860d5eadfaf4d129de4a91ff0f5f4c603
Aaron Kraus <aaron.kraus38@gmail.com>
2023-04-15 14:46:01 -0700

    add more realistic timeouts for ScoreHighAndIntake

--- src/main/java/frc/robot/commands/ScoreHighAndIntake.java
+++ src/main/java/frc/robot/commands/ScoreHighAndIntake.java
@@ -16,12 +16,12 @@ public class ScoreHighAndIntake extends SequentialCommandGroup {
     this.m_arm = arm;
     this.m_intake = intake;
 
-    addCommands(new HighConePosition(this.m_arm).withTimeout(2),
+    addCommands(new HighConePosition(this.m_arm).withTimeout(2.5),
     new ProfiledDriveToPosition(this.m_drive, 23.5),
     new ToggleClaw(this.m_intake), 
     new ProfiledDriveToPosition(this.m_drive, -24),
     new TogglePivot(this.m_arm),
-    new LowerArmAndDrive(this.m_drive, this.m_arm, -71),
+    new LowerArmAndDrive(this.m_drive, this.m_arm, -71).withTimeout(3),
     new RotateToAngle(this.m_drive, 180).withTimeout(0.5),
     new ProfiledDriveToPosition(this.m_drive, 95, 2),
     new IntakeForward(this.m_intake).withTimeout(1),
